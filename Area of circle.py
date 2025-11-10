99d0f0647dc22601cacdee3ae38eae22ec522597
Cholleti bhanu naga venkata thrinadh <bhanucholleti0811@gmail.com>
2025-11-10 21:52:59 +0530

    Add files via upload

---
+++ Area of circle.py
@@ -0,0 +1,5 @@
+#Let's see how to find the area of a circle
+#let's take the radius of the circle
+r=int(input("Enter radius of circle:"))
+Area = 3.14*r*r
+print(f"Area of circle with radius {r} is:{Area}")
---
+++ Area of triangle.py
@@ -0,0 +1,5 @@
+
+b=int(input("Enter breadth of the triangle:"))
+h=int(input("Enter height of the triangle:"))
+area=1/2*b*h
+print(f"Area of triangle with height {h} and breadth {b} is :{area}")
